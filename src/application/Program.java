c161d6b023c111420919a87266524e9878fc3521
Bruno Farias <bruno43ist@yahoo.com.br>
2019-08-13 19:25:34 -0300

    Hashcode

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,6 +10,21 @@ public class Program {
 		
 		System.out.println(a.equals(b));
 		System.out.println(a.equals(c));
+		
+		
+		//MESMO HASHCODE PARA DADOS IGUAIS (a = c = 74113750)
+		//PODE TER COLISÕEX (HASHCODES LIMITADOS A 32BITS)
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
+		System.out.println(c.hashCode());
+		
+		//SE O HASHCODE DE DOIS OBJETOS FOREM DIFERENTES, ENTÃO OS DOIS OBJETOS SÃO DIFERENTES
+		
+		boolean tof = a.hashCode() == b.hashCode();//DARA FALSE
+		boolean tof1 = a.hashCode() == c.hashCode();//DARA TRUE
+		
+		System.out.println(tof);
+		System.out.println(tof1);
 	}
 
 }
