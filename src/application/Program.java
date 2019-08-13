d88c48c05e4b785da19be5a521b5bf58732ab198
Bruno Farias <bruno43ist@yahoo.com.br>
2019-08-13 19:21:29 -0300

    Equals
    wq

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String a = "Maria";
+		String b = "Alex";
+		String c = "Maria";
+		
+		System.out.println(a.equals(b));
+		System.out.println(a.equals(c));
+	}
+
+}
