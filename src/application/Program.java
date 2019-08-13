ac13073842160d312fa5c092f1969e9f14cf05b4
Bruno Farias <bruno43ist@yahoo.com.br>
2019-08-13 19:34:38 -0300

    Gerando automaticamente hashcode e equals

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,30 +1,18 @@
 package application;
 
+import entities.Client;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		String a = "Maria";
-		String b = "Alex";
-		String c = "Maria";
+		Client c1 = new Client("Maria", "maria@gmail.com");
+		Client c2 = new Client("Maria", "maria@gmail.com");
 		
-		System.out.println(a.equals(b));
-		System.out.println(a.equals(c));
-		
-		
-		//MESMO HASHCODE PARA DADOS IGUAIS (a = c = 74113750)
-		//PODE TER COLISÕEX (HASHCODES LIMITADOS A 32BITS)
-		System.out.println(a.hashCode());
-		System.out.println(b.hashCode());
-		System.out.println(c.hashCode());
-		
-		//SE O HASHCODE DE DOIS OBJETOS FOREM DIFERENTES, ENTÃO OS DOIS OBJETOS SÃO DIFERENTES
-		
-		boolean tof = a.hashCode() == b.hashCode();//DARA FALSE
-		boolean tof1 = a.hashCode() == c.hashCode();//DARA TRUE
-		
-		System.out.println(tof);
-		System.out.println(tof1);
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
+		System.out.println(c1 == c2);
 	}
 
 }
