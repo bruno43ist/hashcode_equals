11ba116a201d9a9bdf4a345c9473fb921cc6afca
Bruno Farias <bruno43ist@yahoo.com.br>
2019-08-13 19:37:26 -0300

    Comparando String

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,10 +9,18 @@ public class Program {
 		Client c1 = new Client("Maria", "maria@gmail.com");
 		Client c2 = new Client("Maria", "maria@gmail.com");
 		
+		String s1 = "Test";
+		String s2 = "Test";
+		
+		String s3 = new String("Test");
+		String s4 = new String("Test");
+		
 		System.out.println(c1.hashCode());
 		System.out.println(c2.hashCode());
 		System.out.println(c1.equals(c2));
 		System.out.println(c1 == c2);
+		System.out.println(s1 == s2);//retornará true
+		System.out.println(s3 == s4);//retornará false
 	}
 
 }
